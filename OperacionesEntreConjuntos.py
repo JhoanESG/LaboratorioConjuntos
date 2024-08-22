c5cee7a910afc12eaf836b518d00e4e36f18f1dd
johana <johanapaolapalacio@gmail.com>
2024-08-22 17:07:58 -0500

    Funcion subconjuntos

--- OperacionesEntreConjuntos.py
+++ OperacionesEntreConjuntos.py
@@ -1,3 +1,7 @@
+import random
+import itertools
+
+#OPERACION BASICA UNION DE CONJUNTOS -------------------------------------------------
 def union_conjuntos(*conjuntos):
     # Crear una lista vacía para almacenar la unión de los elementos
     lista_union = []
@@ -28,6 +32,9 @@ print("Unión de los conjuntos en una lista:", resultado)
 
 
 
+
+
+#OPERACION BASICA DIFERENCIA DE CONJUNTOS -------------------------------------------------
 def diferencia_conjuntos(conjunto_base, *conjuntos):
     # Realizar la diferencia de conjuntos
     diferencia = conjunto_base.difference(*conjuntos)
@@ -47,3 +54,29 @@ conjunto3 = {5, 7}
 resultado = diferencia_conjuntos(conjunto_base, conjunto2, conjunto3)
 
 print("Diferencia de los conjuntos en una lista:", resultado)
+
+
+
+#OPERACION BASICA SUBCONJUNTO ALEATORIO -------------------------------------------------
+def subcojunto_aleatorio(*conjuntos):
+    # Seleccionar un conjunto aleatorio de los conjuntos proporcionados
+    conjunto_seleccionado = random.choice(conjuntos)
+    
+    # Obtener un tamaño aleatorio para el subconjunto (entre 0 y el tamaño del conjunto seleccionado)
+    tamaño_subconjunto = random.randint(0, len(conjunto_seleccionado))
+    
+    # Generar un subconjunto aleatorio del tamaño seleccionado
+    subconjunto = set(itertools.islice(random.sample(conjunto_seleccionado, tamaño_subconjunto), tamaño_subconjunto))
+    
+    return conjunto_seleccionado, subconjunto
+
+# Definir varios conjuntos
+conjunto1 = {1, 2, 3, 4, 5}
+conjunto2 = {6, 7, 8, 9}
+conjunto3 = {10, 11, 12}
+
+# Obtener un conjunto aleatorio y un subconjunto de este
+conjunto_aleatorio, subconjunto = subcojunto_aleatorio(conjunto1, conjunto2, conjunto3)
+
+print("Conjunto seleccionado:", conjunto_aleatorio)
+print("Subconjunto aleatorio:", subconjunto)
